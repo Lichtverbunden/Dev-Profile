8665432ed4df608d190a723e0f47e26d76145854
Ken Krippeler <kriken19@gmail.com>
2017-08-15 21:33:57 +0200

    iPhone Auto Layout

--- dev-profile/ViewController.swift
+++ dev-profile/ViewController.swift
@@ -8,16 +8,15 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
-    override func viewDidLoad() {
+class ViewController: UIViewController
+{
+    @IBOutlet weak var logoImage: UIImageView!
+    
+    override func viewDidLoad()
+    {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        
+        logoImage.layer.cornerRadius = 5.0
     }
 
 
